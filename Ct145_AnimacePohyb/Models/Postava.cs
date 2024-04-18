5c390d7ffe7a1a72d80596cd0213b8ed022e9bbf
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-04-18 16:01:04 +0200

    Přidejte soubory projektu.

---
+++ Ct145_AnimacePohyb/Models/Postava.cs
@@ -0,0 +1,21 @@
+﻿namespace Ct145_AnimacePohyb.Models
+{
+    public class Postava
+    {
+        public Postava(string jmeno, string obrazek) 
+        { 
+            Jmeno = jmeno;
+            Obrazek = obrazek;
+            AktualniPozice = 0;
+        }
+        public string Jmeno { get; }
+        public string Obrazek { get; }
+        private int AktualniPozice { get; set; } 
+        private List<Souradnice> PoziceList { get; set; } = new List<Souradnice>();
+        public string Style => PoziceList[AktualniPozice].Style;
+        public void PridejPozici(int pozX, int pozY, int velikostObrazku)
+        {
+            PoziceList.Add(new Souradnice(pozX, pozY, velikostObrazku));
+        }
+    }
+}
